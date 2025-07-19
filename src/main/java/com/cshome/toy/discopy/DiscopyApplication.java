2f2bfc806684e24a2b53842b2ed34d069d89edc5
JangYeongHu <jyhoo040912@gmail.com>
2025-07-19 23:44:07 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/cshome/toy/discopy/DiscopyApplication.java
@@ -0,0 +1,13 @@
+package com.cshome.toy.discopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscopyApplication.class, args);
+	}
+
+}
