0884175ee0efc843dc1796f77155c06c8aba94b9
dkdltm221 <dkdltm221@naver.com>
2025-07-21 18:34:05 +0900

    feat: Member 엔티티 및 MemberRepository 추가

--- src/main/java/com/cshome/toy/discopy/domain/member/Member.java
+++ src/main/java/com/cshome/toy/discopy/domain/member/Member.java
@@ -1,4 +1,29 @@
 package com.cshome.toy.discopy.domain.member;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Builder
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
 }
